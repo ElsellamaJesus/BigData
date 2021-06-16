4fbe151741aa2c38483a5dc333a95030d5f0c2d4
DanielCamacho200 <juan.camacho17@tectijuana.edu.mx>
2021-06-15 20:44:15 -0700

    updating file comments

--- Unit_3/Evaluation/Exam_U3/Examen U3.scala
+++ Unit_3/Evaluation/Exam_U3/Examen U3.scala
@@ -50,15 +50,3 @@ println(s"Within Set Sum of Squared Errors = $WSSSE")
 // Shows the result.
 println("Cluster Centers: ")
 model.clusterCenters.foreach(println)
-
-//**********************
-// Here is the info on the data:
-// 1)   FRESH: annual spending (m.u.) on fresh products (Continuous);
-// 2)   MILK: annual spending (m.u.) on milk products (Continuous);
-// 3)   GROCERY: annual spending (m.u.)on grocery products (Continuous);
-// 4)   FROZEN: annual spending (m.u.)on frozen products (Continuous)
-// 5)   DETERGENTS_PAPER: annual spending (m.u.) on detergents and paper products (Continuous)
-// 6)   DELICATESSEN: annual spending (m.u.)on and delicatessen products (Continuous);
-// 7)   CHANNEL: customers Channel - Horeca (Hotel/Restaurant/Cafe) or Retail channel (Nominal)
-// 8)   REGION: customers Region- Lisnon, Oporto or Other (Nominal)
-//**********************
